1826105d2f731281c92ab515e98cf4a476eab9b1
Nissekissen <nils.m.lindblad@gmail.com>
2022-11-20 12:50:41 +0100

    Added listening

--- api/index.js
+++ api/index.js
@@ -2,3 +2,10 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.get('/api', (req, res) => {
+    res.send("test")
+})
+
+app.listen(port, () => {
+    console.log('API started')
+})
