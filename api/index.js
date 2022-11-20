851dd644b3b1ca335c3ea45aa56e1d37f1d5e524
Nissekissen <nils.m.lindblad@gmail.com>
2022-11-20 17:49:53 +0100

    More error logging

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ router.route('/graph').get(async (req, res) => {
         res.end(await renderer.generateImage('png', '', '', false))
     } catch (error) {
         console.log(error)
-        return res.status(400).json({message: "Bad request."})
+        return res.status(400).json({message: error})
         
     }
     
