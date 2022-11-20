fc83813e27e0c2d113880ab0a2c3f711c14490c3
Nissekissen <nils.m.lindblad@gmail.com>
2022-11-20 16:56:19 +0100

    Added /api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 const port = 10000;
 
-app.get('./api', (req, res) => {
-    res.send("test")
+
+
+router.get('/', (req, res) => {
+    res.json({message: "Welcome to my API"})
 })
+
+app.use('/api', router)
 
 app.listen(port, () => {
     console.log('API started at port ' + port)
